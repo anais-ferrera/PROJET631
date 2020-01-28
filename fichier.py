f92f06410638735de23e0e71d14fd0b8a45d11a2
anais-ferrera <56952574+anais-ferrera@users.noreply.github.com>
2020-01-28 20:50:07 +0100

    Add files via upload

---
+++ fichier.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 23 08:37:27 2020
+
+@author: ferreraa
+"""
+from Arbre import Arbre
+
+class Fichier:
+    
+    def __init__(self,texte):
+        self.texte=texte
+        self.liste_afreq=self.alphabet()
+        self.listeArbre=[]
+        self.longueurTexte=0
+
+    def alphabet(self):
+        fichier = open(self.texte,'r')
+        alphabet = []
+        liste=[]
+        
+        
+        for ligne in fichier:
+            for lettre in ligne:
+                if lettre not in alphabet:
+                    self.longueurTexte=self.longueurTexte+1
+                    alphabet.append(lettre)
+                    frequence= self.frequence(lettre)
+                    liste.append((frequence,lettre))
+                    
+        fichier.close  
+        return sorted(liste)
+        
+
+    def frequence (self,lettre):
+    
+        fichier = open(self.texte,'r')
+        compteur=0
+        
+        for ligne in fichier:
+            for caractere in ligne:
+                if caractere==lettre :
+                    compteur=compteur+1
+        
+        fichier.close
+        return compteur
+    
+    def creation_feuille(self):
+        for (freq,alpha) in self.liste_afreq:
+            self.listeArbre.append(Arbre(freq,alpha))
+            
+    def arbre(self):
+        while(len(self.listeArbre)!=1):
+            self.creationArbre()
+            #self.drawArbre()
+    
+    def indexNewArbre(self):
+        listeA=[]
+        
+        
+        
+        
+    def creationArbre(self):
+        
+        ab1=self.listeArbre[0]
+        ab2=self.listeArbre[1]
+        
+        arbre=Arbre('',ab1.valeur+ab2.valeur,ab2,ab1)
+        
+        self.listeArbre.pop(1)
+        self.listeArbre.pop(0)
+        
+        index=self.indexNewArbre(arbre)
+        self.listeArbre[index:index]=[arbre]
+        
+    def tauxCompression(self):
+        longTexte=self.longueurTexte*8
+        return (1-len(self.arbre)/longTexte)*100
+            
+
+
+texte= 'alice.txt'  
+f=Fichier(texte)
+print(f.alphabet())
+
+
+
+
