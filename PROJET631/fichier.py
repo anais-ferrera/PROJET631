b5cb4d9631c139e0a08ebb9fd14dcfcfce833a95
anais-ferrera <56952574+anais-ferrera@users.noreply.github.com>
2020-01-28 18:59:51 +0100

    Add files via upload

--- PROJET631/fichier.py
+++ PROJET631/fichier.py
@@ -4,52 +4,73 @@ Created on Thu Jan 23 08:37:27 2020
 
 @author: ferreraa
 """
-
+from Arbre import Arbre
 
 class Fichier:
     
     def __init__(self,texte):
         self.texte=texte
+        self.liste_afreq=self.alphabet()
+        self.listeArbre=[]
 
     def alphabet(self):
+        fichier = open(self.texte,'r')
         alphabet = []
-        new_alphabet=[]
+        liste=[]
         
-        for caractere in self.texte :  
-           alphabet.append(caractere)  
-           for i in alphabet:
-               if i not in new_alphabet:
-                   new_alphabet.append(i)
-                   
-        return sorted(new_alphabet)
+        
+        for ligne in fichier:
+            for lettre in ligne:
+                if lettre not in alphabet:
+                    alphabet.append(lettre)
+                    frequence= self.frequence(lettre)
+                    liste.append((frequence,lettre))
+                    
+        fichier.close  
+        return sorted(liste)
         
 
-    def frequence (self):
+    def frequence (self,lettre):
     
-        table_freq = {}
+        fichier = open(self.texte,'r')
+        compteur=0
         
-        for caractere in self.texte:
-            if caractere in table_freq :
-                table_freq[caractere]= table_freq[caractere]+1
-            else :
-                table_freq[caractere]=1
-                
-        return table_freq
+        for ligne in fichier:
+            for caractere in ligne:
+                if caractere==lettre :
+                    compteur=compteur+1
+        
+        fichier.close
+        return compteur
+    
+    def creation_feuille(self):
+        for (freq,alpha) in self.liste_afreq:
+            self.listeArbre.append(Arbre(freq,alpha))
             
-  
+    def arbre(self):
+        while(len(self.listeArbre)!=1):
+            self.creationArbre()
+            #self.drawArbre()
+    
+    def creationArbre(self):
+        
+        ab1=self.listeArbre[0]
+        ab2=self.listeArbre[1]
+        
+        arbre=Arbre('',ab1.valeur+ab2.valeur,ab2,ab1)
+        
+        self.listeArbre.pop(1)
+        self.listeArbre.pop(0)
+        
+        index=self.indexNewArbre(arbre)
+        self.listeArbre[index:index]=[arbre]
+            
 
-t=open('test.txt','r')
-#line = t.readlines()
-#print(line)  
+
+texte= 'alice.txt'  
+f=Fichier(texte)
+print(f.alphabet())
 
 
-t1=open('textesimple.txt','r') 
-test=t1.readline().replace(" ","")   
-
-          
-f=Fichier(t2)
-
-print ('alphabet :',f.alphabet())
-print('fréquence :',f.frequence())
 
 
